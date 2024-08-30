c5e369abdef9cc39940a3377e0fa24b3cb3b3ae0
Dmytro Lukashenko <dmytro.lukashenko@levi9.com>
2024-08-30 19:45:24 +0300

    Divide App.jsx into the separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
 import { CORE_CONCEPTS } from './data'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-const genRandomInt = (max) => {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept({title, description, image}) {
-  return (
-    <li>
-      <img src={image} alt="" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
 
 function App() {
   return (
@@ -42,6 +13,7 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
