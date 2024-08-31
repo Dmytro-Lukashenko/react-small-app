849c151aee2b3303eb807f28eeca2b91d282bde7
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-08-31 14:29:18 +0300

    Rendering content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,21 @@ import TabButton from './components/TabButton/TabButton';
 import { EXAMPLES } from './data.js'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState(null)
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
   }
+
+  let tabContent = !selectedTopic ? <p>Please select a topic</p> :
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
 
   return (
     <div>
@@ -32,15 +43,7 @@ function App() {
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
         <h2>Time to get started!</h2>
       </main>
