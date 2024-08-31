e79535284e26753151dc4dfec68f50f632e1a84b
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-08-31 16:00:11 +0300

    Provide a default value for the ButtonsContainer props

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -26,7 +26,6 @@ export default function Examples() {
     return (
         <Section title="Examples" id="examples">
             <Tabs
-                ButtonsContainer="menu"
                 buttons={
                     <>
                         <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
