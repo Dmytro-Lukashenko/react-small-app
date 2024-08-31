d09dbd129bb223429b95a1917f5073a0240ea9fb
Dmytro Lukashenko <dmytro.lukashenko@levi9.com>
2024-08-31 12:22:16 +0300

    Passing custom arguments to event functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
 function App() {
 
-  function handleClick() {
-    console.log('click')
+  function handleClick(selectedValue) {
+    console.log(selectedValue)
   }
 
   return (
@@ -24,10 +24,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
           Dynamic content
         </section>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,8 +1,4 @@
 export default function TabButton({ children, onClick }) {
-    function handleClick() {
-        console.log('click')
-    }
-
     return (
         <li>
             <button onClick={onClick}>{children}</button>
